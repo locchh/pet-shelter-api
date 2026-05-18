11d6248d07440f4aa5ba41d72adee71b239f53b4
LocCH <locc7196@gmail.com>
2026-05-18 15:59:35 +0700

    feat: add Express server entry point on port 8000

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import type {Express} from 'express'
+
+const PORT = 8000
+const app:Express = express()
+
+app.listen(PORT, ():void=>{
+  console.log("Listening on port: ", PORT)
+})
