d44cbaea444d955a31ea1bbe9d2c317ee013d4cb
LocCH <locc7196@gmail.com>
2026-05-18 16:36:21 +0700

    1. Create an object called `pet` before the `/` route that includes:    - name (string)    - species (string)    - adopted (boolean)    - age (number) 2. Type your new pet object (inline or using a custom type) 3. Update your existing GET `/` route to return that pet object

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,24 @@ import type {Express} from 'express'
 const PORT = 8000
 const app:Express = express()
 
-app.get('/', (req, res) => {
-  res.json({})
+type Pet = {
+  name: string,
+  species: string,
+  adopted: boolean,
+  age: number
+}
+
+const pet:Pet = {
+  name: "Rubik",
+  species: "Cat",
+  adopted: true,
+  age: 3
+}
+
+app.get('/', (req, res)=> {
+  res.json(pet)
 })
 
-app.listen(PORT, ():void=>{
+app.listen(PORT, ():void =>{
   console.log("Listening on port: ", PORT)
 })
