ed8fccf48641dd2bc886747fd59afae22edbcc55
LocCH <locc7196@gmail.com>
2026-05-18 19:41:34 +0700

    refactor: move route logic into dedicated controllers module

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,62 +1,10 @@
 import express from 'express'
-import type {Router, Request, Response} from 'express'
+import type {Router} from 'express'
 
-import type {Pet} from '../data/pets'
-import {pets} from '../data/pets'
+import {getPets, getPetById} from '../controllers/pets.controllers'
 
 export const petRouter:Router = express.Router()
 
-type PetQueryParams = {
-  species?:string,
-  adopted?: 'true' | 'false',
-  minAge?: string,
-  maxAge?: string
-}
+petRouter.get('/', getPets)
 
-petRouter.get('/', (
-  req:Request<{}, unknown, {}, PetQueryParams>, 
-  res:Response<Pet[]>
-):void=> {
-  const {species, adopted, minAge, maxAge} = req.query
-
-  let filteredPets:Pet[] = pets
-  
-  if (species){
-    filteredPets = filteredPets.filter((pet:Pet):boolean=>
-      pet.species.toLowerCase() === species.toLowerCase()
-    )
-  }
-
-  if (adopted){
-    filteredPets = filteredPets.filter((pet:Pet):boolean=>
-      pet.adopted === JSON.parse(adopted)
-    )
-  }
-
-  if (minAge){
-    filteredPets = filteredPets.filter((pet:Pet):boolean=>
-      pet.age >= JSON.parse(minAge)
-    )
-  }
-
-  if (maxAge){
-    filteredPets = filteredPets.filter((pet:Pet):boolean=>
-      pet.age <= JSON.parse(maxAge)
-    )
-  }
-
-  res.json(filteredPets)
-})
-
-petRouter.get('/:id', (
-  req:Request<{id:string}>,
-  res:Response<Pet|{message:string}>):void=>{
-  const {id} = req.params
-  const pet:Pet|undefined = pets.find((pet:Pet):boolean => pet.id.toString() === id)
-  
-  if (pet){
-    res.json(pet)
-  } else {
-    res.status(404).json({message: "No pet with that ID"})
-  }
-})
+petRouter.get('/:id', getPetById)
